a32d0b9c9f855ffcba96a1bec8123ddadbaf1f9e
Aminu Abdulsalami <lecturer34@gmail.com>
2020-12-31 16:12:13 +0100

    Fixed bugs with department CRUD

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SchoolController;
+use App\Http\Controllers\DepartmentController;
 
 
 Route::get('/', function(){
@@ -15,3 +16,12 @@ Route::get('schools/edit/{id}', [SchoolController::class, 'edit'])->name('school
 Route::post('schools/update', [SchoolController::class, 'update'])->name('school.update');
 Route::get('schools/delete/{id}', [SchoolController::class, 'delete'])->name('school.delete');
 Route::post('schools/destroy', [SchoolController::class, 'destroy'])->name('school.destroy');
+
+
+Route::get('departments', [DepartmentController::class, 'index'])->name('department.list');
+Route::get('departments/create', [DepartmentController::class, 'create'])->name('department.create');
+Route::post('departments/store', [DepartmentController::class, 'store'])->name('department.store');
+Route::get('departments/edit/{id}', [DepartmentController::class, 'edit'])->name('department.edit');
+Route::post('departments/update', [DepartmentController::class, 'update'])->name('department.update');
+Route::get('departments/delete/{id}', [DepartmentController::class, 'delete'])->name('department.delete');
+Route::post('departments/destroy', [DepartmentController::class, 'destroy'])->name('department.destroy');
